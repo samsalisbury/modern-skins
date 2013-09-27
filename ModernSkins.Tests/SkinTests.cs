336e12f2bc44f5373898fb8e9fb0135f786a6c79
Sam Salisbury <samsalisbury@gmail.com>
2013-09-27 11:08:06 +0100

    Test cleanup

--- ModernSkins.Tests/SkinTests.cs
+++ ModernSkins.Tests/SkinTests.cs
@@ -13,32 +13,24 @@ namespace ModernSkins.Tests
             Assert.Throws<DirectoryNotFoundException>(() => new Skin(nonExistentPath));
         }
 
-        [Test]
-        public void Ctor_Throws_DirectoryNotFound_IfPassedPathToFile()
+        [TestCase("~/Skins/testskin/styles/other_styles.less")]
+        [TestCase("~/Skins/testskin2/styles/some_scss_styles2.scss")]
+        public void Ctor_Throws_DirectoryNotFound_IfPassedPathToFile(string appRelativeFileDir)
         {
-            var filePath = TestHelper.ResolveAppDir("~/Skins/testskin/styles/other_styles.less");
+            var filePath = TestHelper.ResolveAppDir(appRelativeFileDir);
 
             Assert.Throws<DirectoryNotFoundException>(() => new Skin(filePath));
         }
 
-        [Test]
-        public void Ctor_GivesSkinCorrectName()
+        [TestCase("~/Skins/testskin", "testskin")]
+        [TestCase("~/Skins/testskin2", "testskin2")]
+        public void Ctor_GivesSkinCorrectName(string appRelativeSkinDir, string expectedName)
         {
-            var testSkinDir = TestHelper.ResolveAppDir("~/Skins/testskin");
+            var testSkinDir = TestHelper.ResolveAppDir(appRelativeSkinDir);
 
             var skin = new Skin(testSkinDir);
 
-            Assert.That(skin.Name, Is.EqualTo("testskin"));
-        }
-
-        [Test]
-        public void Ctor_ReallyGivesSkinCorrectName()
-        {
-            var testSkinDir = TestHelper.ResolveAppDir("~/Skins/testskin2");
-
-            var skin = new Skin(testSkinDir);
-
-            Assert.That(skin.Name, Is.EqualTo("testskin2"));
+            Assert.That(skin.Name, Is.EqualTo(expectedName));
         }
     }
 }
