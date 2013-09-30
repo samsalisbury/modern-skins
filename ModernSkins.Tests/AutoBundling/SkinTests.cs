2ab6102f4a7e66ecd9b5789507b9725f3fc3e8c9
Sam Salisbury <samsalisbury@gmail.com>
2013-09-30 18:52:34 +0100

    Removed System.IO from SkinTests.cs

--- ModernSkins.Tests/AutoBundling/SkinTests.cs
+++ ModernSkins.Tests/AutoBundling/SkinTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using ModernSkins.AutoBundling;
@@ -13,53 +14,98 @@ namespace ModernSkins.Tests.AutoBundling
         [TestCase("C:\\Nor\\is\\this")]
         public void Ctor_Throws_DirectoryNotFound_IfPassedNonExistentPath(string nonExistentPath)
         {
-            Assert.Throws<DirectoryNotFoundException>(() => new Skin(nonExistentPath, new FileSystem()));
+            var fs = new FakeFileSystem();
+
+            Assert.Throws<DirectoryNotFoundException>(() => new Skin(nonExistentPath, fs));
         }
 
-        [TestCase("~/Skins/testskin/styles/other_styles.less")]
-        [TestCase("~/Skins/testskin2/styles/some_scss_styles2.scss")]
-        public void Ctor_Throws_DirectoryNotFound_IfPassedPathToFile(string appRelativeFilePath)
+        [TestCase("/Skins/testskin/styles/other_styles.less")]
+        [TestCase("/Skins/testskin2/styles/some_scss_styles2.scss")]
+        public void Ctor_Throws_DirectoryNotFound_IfPassedPathToFile(string filePath)
         {
-            var filePath = TestHelper.ResolveAppDir(appRelativeFilePath);
+            var fs = new FakeFileSystem();
+            fs.AddFile(filePath);
 
-            Assert.Throws<DirectoryNotFoundException>(() => new Skin(filePath, new FileSystem()));
+            Assert.Throws<DirectoryNotFoundException>(() => new Skin(filePath, fs));
         }
 
-        [TestCase("~/Skins/testskin", "testskin")]
-        [TestCase("~/Skins/testskin2", "testskin2")]
-        public void Ctor_GivesSkinCorrectName(string appRelativeSkinDir, string expectedName)
+        [TestCase("/Skins/testskin", "testskin")]
+        [TestCase("/Skins/testskin2", "testskin2")]
+        public void Ctor_GivesSkinCorrectName(string skinDir, string expectedName)
         {
-            var testSkinDir = TestHelper.ResolveAppDir(appRelativeSkinDir);
+            var fs = new FakeFileSystem();
+            fs.AddDirectory(skinDir);
 
-            var skin = new Skin(testSkinDir, new FileSystem());
+            var skin = new Skin(skinDir, fs);
 
             Assert.That(skin.Name, Is.EqualTo(expectedName));
         }
 
-        [Category("FileSystem")]
-        [TestCase("~/Skins/testskin", "bundle_a", typeof(ScriptAutoBundle), "~/testskin/scripts/bundle_a")]
-        [TestCase("~/Skins/testskin", "bundle_b", typeof(ScriptAutoBundle), "~/testskin/scripts/bundle_b")]
-        [TestCase("~/Skins/testskin", "js_bundle", typeof(ScriptAutoBundle), "~/testskin/scripts/js_bundle")]
-        [TestCase("~/Skins/testskin", "another_js_bundle", typeof(ScriptAutoBundle), "~/testskin/scripts/another_js_bundle")]
-        [TestCase("~/Skins/testskin", "other_styles", typeof(StyleAutoBundle), "~/testskin/styles/other_styles")]
-        [TestCase("~/Skins/testskin", "some_base_styles", typeof(StyleAutoBundle), "~/testskin/styles/some_base_styles")]
-        [TestCase("~/Skins/testskin", "some_scss_styles", typeof(StyleAutoBundle), "~/testskin/styles/some_scss_styles")]
-        //[TestCase("~/Skins/testskin2", "other_styles2", typeof(StyleAutoBundle), "~/testskin2/styles/other_styles2")]
-        //[TestCase("~/Skins/testskin2", "some_base_styles2", typeof(StyleAutoBundle), "~/testskin2/styles/some_base_styles2")]
-        //[TestCase("~/Skins/testskin2", "some_scss_styles2", typeof(StyleAutoBundle), "~/testskin2/styles/some_scss_styles2")]
-        public void CreateBundles_ReturnsExpectedBundles(string skinPath, string bundleName, Type bundleType, string virtualPath)
+        [Test]
+        public void CreateBundles_ReturnsExpectedBundles()
         {
-            var basePath = TestHelper.ResolveAppDir("~/Skins");
-            var testSkinDir = TestHelper.ResolveAppDir(skinPath);
-            var skin = new Skin(testSkinDir, new FileSystem());
-            
+            const string basePath = "/skins";
+            const string testSkinDir = "/skins/my-skin";
+
+            var fs = new FakeFileSystem();
+
+            var expectations = new Dictionary<string, Tuple<string, Type>>();
+
+            // Scripts
+            fs.AddFile("/skins/my-skin/scripts/crazy_bundle/file1.js");
+            fs.AddFile("/skins/my-skin/scripts/crazy_bundle/file2.js");
+            fs.AddFile("/skins/my-skin/scripts/crazy_bundle/dirs_inside_scripts_are_bundles.js");
+            expectations.Add("crazy_bundle", new Tuple<string, Type>("~/my-skin/scripts/crazy_bundle", typeof (ScriptAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/scripts/bundlino/file_z.js");
+            expectations.Add("bundlino", new Tuple<string, Type>("~/my-skin/scripts/bundlino", typeof (ScriptAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/scripts/scripterific.js");
+            expectations.Add("scripterific", new Tuple<string, Type>("~/my-skin/scripts/scripterific", typeof (ScriptAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/scripts/java.coffee");
+            expectations.Add("java", new Tuple<string, Type>("~/my-skin/scripts/java", typeof (ScriptAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/scripts/all_files_directly_in_scripts_are_separate_bundles.coffee");
+            expectations.Add("all_files_directly_in_scripts_are_separate_bundles",
+                new Tuple<string, Type>("~/my-skin/scripts/all_files_directly_in_scripts_are_separate_bundles", typeof (ScriptAutoBundle)));
+
+            // Styles
+            fs.AddFile("/skins/my-skin/styles/my_lovely_styles.css");
+            expectations.Add("my_lovely_styles", new Tuple<string, Type>("~/my-skin/styles/my_lovely_styles", typeof (StyleAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/styles/my_sytlings.sass");
+            expectations.Add("my_sytlings", new Tuple<string, Type>("~/my-skin/styles/my_sytlings", typeof (StyleAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/styles/my_rad_stiles.scss");
+            expectations.Add("my_rad_stiles", new Tuple<string, Type>("~/my-skin/styles/my_rad_stiles", typeof (StyleAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/styles/not_much.less");
+            expectations.Add("not_much", new Tuple<string, Type>("~/my-skin/styles/not_much", typeof (StyleAutoBundle)));
+
+            fs.AddFile("/skins/my-skin/styles/some-lib/a_lib_file.less");
+            fs.AddFile("/skins/my-skin/styles/some-lib/another_file_in_a_lib.less");
+            fs.AddFile("/skins/my-skin/styles/some-lib/lib_files_do_not_show_up_as_bundles.css");
+            fs.AddFile("/skins/my-skin/styles/some-lib/and_nor_do_their_folders.css");
+            // Note: The above stanza adds no expectations, since it's a lib in the styles folder :)
+
+            var skin = new Skin(testSkinDir, fs);
+
             var bundles = skin.CreateBundles();
 
-            Assert.That(bundles, Has.Length.EqualTo(7));
-            var bundle = bundles.SingleOrDefault(b => b.VirtualPath(basePath) == virtualPath);
-            Assert.That(bundle, Is.Not.Null, "Bundle {0} not found.", virtualPath);
-            Assert.That(bundle, Is.TypeOf(bundleType));
-            Assert.That(bundle.Name, Is.EqualTo(bundleName));
+            Assert.That(bundles, Has.Length.EqualTo(expectations.Count));
+
+            foreach (var expectation in expectations)
+            {
+                var expectedName = expectation.Key;
+                var expectedVirtualPath = expectation.Value.Item1;
+                var expectedType = expectation.Value.Item2;
+
+                var bundle = bundles.SingleOrDefault(b => b.VirtualPath(basePath) == expectedVirtualPath);
+                Assert.That(bundle, Is.Not.Null, "Bundle {0} not found.", expectedVirtualPath);
+                Assert.That(bundle, Is.TypeOf(expectedType));
+                Assert.That(bundle.Name, Is.EqualTo(expectedName));
+            }
         }
     }
 }
