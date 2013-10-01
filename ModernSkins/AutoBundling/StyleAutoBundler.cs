fcc24a837cbc0f0b94a87be925e742d3328b579e
Sam Salisbury <samsalisbury@gmail.com>
2013-10-01 18:51:38 +0100

    Extract file

--- ModernSkins/AutoBundling/StyleAutoBundler.cs
+++ ModernSkins/AutoBundling/StyleAutoBundler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace ModernSkins.AutoBundling
@@ -40,19 +39,6 @@ namespace ModernSkins.AutoBundling
                     bundleDictionary.Add(bundle.Name, bundle);
                 }
             }
-        }
-    }
-
-    public class StyleBundleWithSameNameException : Exception
-    {
-        public StyleBundleWithSameNameException(AutoBundleBase existingBundle, AutoBundleBase newBundle)
-            : base(string.Format("Cannot add {0}, there is already a bundle with the same name, from path {1}. " +
-            "Each bundle is named after its filename without extension, so if you have a file called " +
-            "'mybundle.less' and another called 'mybundle.scss' they will conflict. The exception to this rule " +
-            "is CSS files, which will be silently ignored if they conflict. This is to allow tools to generate " +
-            "these files at design time without interfering with debugging.",
-            newBundle.FileSystemPath, existingBundle.FileSystemPath))
-        {
         }
     }
 }
---
+++ ModernSkins/AutoBundling/StyleBundleWithSameNameException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ModernSkins.AutoBundling
+{
+    public class StyleBundleWithSameNameException : Exception
+    {
+        public StyleBundleWithSameNameException(AutoBundleBase existingBundle, AutoBundleBase newBundle)
+            : base(string.Format("Cannot add {0}, there is already a bundle with the same name, from path {1}. " +
+                                 "Each bundle is named after its filename without extension, so if you have a file called " +
+                                 "'mybundle.less' and another called 'mybundle.scss' they will conflict. The exception to this rule " +
+                                 "is CSS files, which will be silently ignored if they conflict. This is to allow tools to generate " +
+                                 "these files at design time without interfering with debugging.",
+                newBundle.FileSystemPath, existingBundle.FileSystemPath))
+        {
+        }
+    }
+}
