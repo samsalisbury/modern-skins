cb53b92875bf333c9dbe3932077a75df2e384dc1
Sam Salisbury <samsalisbury@gmail.com>
2013-10-01 11:44:16 +0100

    Remove System.IO from StyleAutoBundlerTests

--- ModernSkins.Tests/AutoBundling/StyleAutoBundlerTests.cs
+++ ModernSkins.Tests/AutoBundling/StyleAutoBundlerTests.cs
@@ -6,21 +6,25 @@ namespace ModernSkins.Tests.AutoBundling
     [TestFixture]
     public class StyleAutoBundlerTests
     {
-        [TestCase("some_base_styles", "~/Skins/testskin/styles/some_base_styles.css")]
-        [TestCase("some_scss_styles", "~/Skins/testskin/styles/some_scss_styles.scss")]
-        [TestCase("other_styles", "~/Skins/testskin/styles/other_styles.less")]
-        public void GetStyleBundles_ReturnsExpectedBundles(string name, string path)
+        [TestCase("some_base_styles", "/my-skin/styles/some_base_styles.css")]
+        [TestCase("some_scss_styles", "/my-skin/styles/some_scss_styles.scss")]
+        [TestCase("other_styles", "/my-skin/styles/other_styles.less")]
+        public void GetStyleBundles_ReturnsExpectedBundles_IgnoringDirectories(string name, string fileToCreate)
         {
-            var styleDirPath = TestHelper.ResolveAppDir("~/Skins/testskin/styles");
-            var bundler = new StyleAutoBundler(styleDirPath, new FileSystem());
+            var fs = new FakeFileSystem();
+            fs.AddFile(fileToCreate);
+            fs.AddDirectory("/my-skin/styles/this_dir_should_be_ignored");
+            
+            const string styleDirPath = "/my-skin/styles";
+
+            var bundler = new StyleAutoBundler(styleDirPath, fs);
 
             var styles = bundler.GetStyleBundles();
 
-            Assert.That(styles, Has.Count.EqualTo(3));
-
+            Assert.That(styles, Has.Count.EqualTo(1));
             Assert.That(styles.Keys, Contains.Item(name));
             Assert.That(styles[name].Name, Is.EqualTo(name));
-            Assert.That(styles[name].FileSystemPath, Is.EqualTo(TestHelper.ResolveAppDir(path)));
+            Assert.That(styles[name].FileSystemPath, Is.EqualTo(fileToCreate));
         }
     }
 }
