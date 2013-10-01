cd8762b7414cc016f27a860149633b1af69bc4bc
Sam Salisbury <samsalisbury@gmail.com>
2013-10-01 12:00:29 +0100

    Fix tests

--- ModernSkins.Tests/AutoBundling/AutoBundlerBaseTests.cs
+++ ModernSkins.Tests/AutoBundling/AutoBundlerBaseTests.cs
@@ -10,12 +10,21 @@ namespace ModernSkins.Tests.AutoBundling
         [TestCase("X:\\MySln\\MyApp\\Skins\\skin_name\\scripts\\bundle", "X:\\MySln\\MyApp\\Skins", "~/skin_name/scripts/bundle")]
         [TestCase("X:\\MySln\\MyApp\\Skins\\skin_name\\scripts\\bundle.extension", "X:\\MySln\\MyApp\\Skins", "~/skin_name/scripts/bundle")]
         [TestCase("X:\\MySln\\MyApp\\Skins\\skin.name\\scripts\\bundle", "X:\\MySln\\MyApp\\Skins", "~/skin.name/scripts/bundle")]
+        public void VirtualPath_ReturnsTheExpectedVirtualPath_Dos(string filePath, string skinsPath, string expectedPath)
+        {
+            var bundler = (AutoBundleBase) new TestAutoBundler(filePath, new FakeDosFileSystem());
+
+            var result = bundler.VirtualPath(skinsPath);
+
+            Assert.That(result, Is.EqualTo(expectedPath));
+        }
+
         [TestCase("/user/MySln/MyApp/Skins/skin_name/scripts/bundle", "/user/MySln/MyApp/Skins", "~/skin_name/scripts/bundle")]
         [TestCase("/user/MySln/MyApp/Skins/skin_name/scripts/bundle.extension", "/user/MySln/MyApp/Skins", "~/skin_name/scripts/bundle")]
         [TestCase("/user/MySln/MyApp/Skins/skin.name/scripts/bundle", "/user/MySln/MyApp/Skins", "~/skin.name/scripts/bundle")]
-        public void VirtualPath_ReturnsTheExpectedVirtualPath(string filePath, string skinsPath, string expectedPath)
+        public void VirtualPath_ReturnsTheExpectedVirtualPath_Unix(string filePath, string skinsPath, string expectedPath)
         {
-            var bundler = (AutoBundleBase) new TestAutoBundler(filePath);
+            var bundler = (AutoBundleBase) new TestAutoBundler(filePath, new FakeUnixFileSystem());
 
             var result = bundler.VirtualPath(skinsPath);
 
@@ -25,7 +34,7 @@ namespace ModernSkins.Tests.AutoBundling
 
     public class TestAutoBundler : AutoBundleBase
     {
-        public TestAutoBundler(string path) : base(path, new Mock<IFileSystem>().Object)
+        public TestAutoBundler(string path, IFileSystem fileSystem) : base(path, fileSystem)
         {
         }
     }
