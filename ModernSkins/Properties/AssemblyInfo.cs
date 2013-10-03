f4a900c81faa629ea1087011a6c843d005744f8e
Sam Salisbury <samsalisbury@gmail.com>
2013-10-03 14:35:04 +0100

    Create nuget package http://www.nuget.org/packages/ModernSkins

--- ModernSkins/Properties/AssemblyInfo.cs
+++ ModernSkins/Properties/AssemblyInfo.cs
@@ -6,14 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ModernSkins")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Auto bundling and minification for ASP.NET MVC applications.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sam Salisbury")]
 [assembly: AssemblyProduct("ModernSkins")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
